c24afd10a91dd0eb869409a47e6f2a3cb2186268
Jay Danagashiya <pjay22756@gmail.com>
2025-02-24 14:14:39 +0530

    update

---
+++ condion.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=10;
+    if(n%2==0)
+    {
+        cout<<n<<"even number"<<endl;
+    }
+    else
+    {
+        cout<<n<<"even number"<<endl;
+    }
+    return 0;
+}
---
+++ condion2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    int b = 20;
+
+    if (a > b)
+    {
+        cout << "a is bigger than b" << endl;
+    }
+    else
+    {
+        cout << "b is bigger than a" << endl;
+    }
+    return 0;
+}
